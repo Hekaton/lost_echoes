8dc944097bb4073101dbc3ce52cfed5c2fffa903
Dmitri Kolytchev <dmitri@hekaton.studio>
2020-02-01 17:39:22 -0500

    Adding materials and shit

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private SymbolRenderer symbolRenderer;
+    
     public void GotToScene(int scene){
         SceneManager.LoadScene(scene);
+    }
+    
+    void Start(){
+        SymbolRenderer.dataRendered += FinalizeSymbol_step2;
+    }
+    
+    public void FinalizeSymbol(){
+        symbolRenderer.GrabTexture(); // will fire async dataRendered event
+    }
+    
+    void FinalizeSymbol_step2(Color[] canvas){
+        Texture2D currentStrokeTexture = new Texture2D(1, 1, TextureFormat.ARGB32, 0, true);
+        var result = Grid2DUtil.CompareGrid(
+            currentStrokeTexture.GetPixels(0, 0, currentStrokeTexture.width, currentStrokeTexture.height),
+            canvas
+        );
+        
+        Debug.LogFormat("Got a result of {0}%", Mathf.FloorToInt((float) result * 100));
     }
 }
