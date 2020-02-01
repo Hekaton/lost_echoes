87b797e49aa2ac161ef2307ee05111d19279cca5
Dmitri Kolytchev <dmitri@hekaton.studio>
2020-01-31 23:02:25 -0500

    Adding .gitkeep and a bunch of other garbage

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Display.displays[0].Activate();
+        Display.displays[1].Activate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
