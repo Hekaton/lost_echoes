f9365040c38ee702f336b85ecad5fcf1fc53f61d
Isabelle Tremblay <isabelle.trem@outlook.com>
2020-02-02 13:17:16 -0500

    New Script

---
+++ Assets/Scripts/ScoresTable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoresTable : MonoBehaviour
+{
+
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    
+    private void Awake() {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
