23276021ea5a28666c9ab7bac2acfaa82dbf58e9
Isabelle Tremblay <isabelle.trem@outlook.com>
2020-02-02 01:00:24 -0500

    new stuff before sleep

---
+++ Assets/buttonshower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonshower : MonoBehaviour
+{
+    public GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void showbutton(){
+        button.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
